38429b8460f638bc952b88ebce62c2763989528a
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2022-05-22 14:59:13 +0530

    Create inputs.tf

---
+++ inputs.tf
@@ -0,0 +1,11 @@
+variable "identifier" {}
+variable "subnet_ids" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "storage_type" {}
+variable "allocated_storage" {}
+variable "vpc_security_group_ids" {}
+variable "db_name" {}
+variable "password" {}
+variable "username" {}
